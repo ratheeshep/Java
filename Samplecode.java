c40cd126172ada90178bf31e510229ff1b7c5ab5
Vishwanathms <vishwanath.murthy@gmail.com>
2019-07-22 07:21:13 +0530

    Update Samplecode.java

--- Samplecode.java
+++ Samplecode.java
@@ -1,5 +1,5 @@
 class Simple{  
-    public static void main(String args[]){  
+    public static void main(String args[]) throws InterruptedException{  
      Thread.sleep(5000);
      System.out.println("Hello Java");  
     }  
