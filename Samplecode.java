8b596fdc4e4e6a476e5b7a458878ab6af22aa249
ratheeshep <51735534+ratheeshep@users.noreply.github.com>
2019-08-27 13:43:23 +0530

    Update Samplecode.java

--- Samplecode.java
+++ Samplecode.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]) throws InterruptedException{  
      Thread.sleep(5000);
-     System.out.println("Hello Java updated aug 27 1.30 PM");  
+     System.out.println("Hello Java updated aug 27 1.40 PM");  
     }  
 }
